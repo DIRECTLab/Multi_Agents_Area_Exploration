2824282eca887082fbf07134fbb4aa25f9aee749
hmk <huzeyfeekocabas@gmail.com>
2023-02-09 17:34:23 -0700

    finish optimization round

--- bidding.py
+++ bidding.py
@@ -59,19 +59,19 @@ class agent(Box):
             if t[0] < 0 or t[1] < 0 or t[0] >= ROWS or t[1] >= COLUMNS:
                 pn[i] = None
             else:
+                # if the neighbor within the grid, then calculate distance towards those agents
                 dist = np.sqrt((self.row - t[0])**2 + (self.column - t[1])**2)
+                # and generate the neighbors for each agent(key:value --> (row,column):distance)
                 self.neighbors[(t[0], t[1])] = dist
         
         # calculate the movable cells for an agent by removing "agent_locations and wall_locs" from the full neighbor list
         subtract_res = {k:[v] for k,v in self.neighbors.items() if(k not in all_agent_locations and k not in wall_locs)}
         self.neighbors.clear()
+        # new neighbors for the agents now is subtract_res
         self.neighbors = subtract_res
         
         
         
-
-
-    
     # calculate the distance matrix(full grid) for each agent based on the agent's location
     def calc_distance_matrices(self):
         x_arr, y_arr = np.mgrid[0:ROWS, 0:COLUMNS]
@@ -92,11 +92,12 @@ class agent(Box):
         # If the agent only has one neighbor, offer a bid based on the neighbor is visited or not
         if(len(self.neighbors)==1):
             for key, value in self.neighbors.items():
+                # if there is only one neighbor, which is previously visited, then offer a bid (0)
                 if(grid[key[0]][key[1]].agent == True):
-                    self.neighbors[key].append(0)
+                    self.neighbors[key].extend([0, self.agent_id])
+                # else: use the following formula to bid to a neighbor
                 else:
-                    self.neighbors[key].append((1/self.start_energy) * (1 / (0.5 * value[0])))
-
+                    self.neighbors[key].extend([(1/self.start_energy) * (1 / (0.5 * value[0])), self.agent_id])
         # If an agent has more than one neighbor, offer a bid for each neighbors based on the neighbors are visited or not
         else:
             # use the following count variable for the following purpose:
@@ -105,54 +106,38 @@ class agent(Box):
             count = 0
             # copy the self.neighbors dictionary to prevent runtime error since we are adding new elements to the neighbors dictionary during the iteration
             neighbors_copy = tuple(self.neighbors.items())
-
             # for each neighbor of an agent, check their neighbors whether a neighbor is previosly visited or not
             for key, value in neighbors_copy:
-                # print("Agent", self.agent_id, "has following neighbors:", self.neighbors)
-                
                 # if a neighbor is previosly visited, offer something less
                 if(grid[key[0]][key[1]].agent == True):
-                    # print("*** 333 ***")
-                    self.neighbors[key].append(0)
-                    # print("Agent",self.agent_id, "is going to offer for the neighbor",key[0],key[1],"with the bid:",0)
-                    
+                    self.neighbors[key].extend([0, self.agent_id])
                     # if all neighbors for the agent are previously visited or has an agent in one one of the neighbor locs, then bid to other cells that are not visited
                     count = count + 1
                     if(count == len(self.neighbors)):
-                        # print("YEAP It worked for agent", self.agent_id, "with having all", count, "neighbors are previously visited!")
                         # bring visited cells ==> (visited + agent_locs)
                         visited_cells = bring_all_visited_locations()
                         # print("For agent",self.agent_id,"located at", self.row, self.column, "here are the all visited cells:", visited_cells)
-                        
                         # for the agent, calculate distance matrix for all grid
                         self.distance_matrix = self.calc_distance_matrices()
                         # print(self.distance_matrix)
-                        
                         # mark the visited cell locations as "None" since we are interested in to find lowest cost unvisited cell
                         for i in visited_cells:
                             row_c,column_c = i[0],i[1]
                             self.distance_matrix[row_c][column_c] = None
-                        
-                        # if the distance matrix if full of "nan" values, then "all_areas_explored" flag will turn to TRUE
+                        # if the distance matrix is full of "nan" values, then "all_areas_explored" flag will turn to TRUE
                         if(np.isnan(self.distance_matrix).all()):
                             all_areas_explored = True
                             break
-
                         # below two lines are getting the min value from unvisited cells and assign them into the surrounded agent
                         # min_value_among_unv_cells = np.unravel_index(np.nanargmin(self.distance_matrix, axis=None), self.distance_matrix.shape)
                         # self.neighbors[min_value_among_unv_cells] = [np.nanmin(self.distance_matrix), ((1/self.start_energy) * ((1 / np.nanmin(self.distance_matrix)) + 1))]
-                        
                 # if the neighbor is previously not visited, then offer something higher
                 elif(grid[key[0]][key[1]].agent == False):
-                    # print("*** 444 ***")
-                    self.neighbors[key].append((1/self.start_energy) * (1 / value[0]))
-                    # print("Agent",self.agent_id, "is going to offer for the neighbor",key[0],key[1],"with the bid:",[(1/self.start_energy) * (1 / value[0])])
+                    self.neighbors[key].extend([(1/self.start_energy) * (1 / value[0]), self.agent_id])
     # this function send a zero bid to a neighbor when it does not have an energy left
     def zero_bid(self):
         for key, value in self.neighbors.items():
-            # print("*** 666 ***")
-            self.neighbors[key].append(0)
-            # print("Agent",self.agent_id, "bids:", 0, "since it does not have any enery left.")
+            self.neighbors[key].extend([0, self.agent_id])
 
 def bring_all_visited_locations():
     # print("\n\nwhere are the zeros:")
@@ -235,48 +220,42 @@ def main():
             # based on the entered keyboard key, run different things
             if event.type == pygame.KEYDOWN:
                 
+
+                
                 # if the user clicks n, run followings
                 if event.key == pygame.K_n:
-                        # what is it for: if an agent does not win any cell, increase empty_dic_counter
+                        # if an agent does not win any cell, increase empty_dic_counter
                         # if nobody wins anything, then it is one of the condition to end the program
                         empty_dic_counter = 0
 
-                        # for each agent in agent_list: find its neighbors, calculate distance to each neighbor, remove the wall & agent locs from total_neighbor_list
+
+
+                        # for each agent in agent_list: find its neighbors, calculate distance to each neighbor, remove the wall & agent locs from neighbor_list
                         for i in agent_list:
                             agent_list[i].find_my_neighbours(agent_locs, wall_list)
-                            # print("agent_list[i].neighbors", agent_list[i].neighbors)
+                            
                             if(agent_list[i].start_energy > 0):
                                 agent_list[i].send_bid()
                             else:
                                 # print("You are negative. You cannot bid.")
                                 agent_list[i].zero_bid()
                         
-                            
-                            
-                        
-
-                        
-
-                        # for each neighbor cell: list the bids
-                        # ex: for (1,1) cells which agents offered a bid and which amount
-                        # put all these "agent_list[i].neighbors" into one dictionary with their agent IDs
-                        
-                        for i in agent_list:
-                            # print("agent_list[i].neighbors",agent_list[i].neighbors)
-                            for j in range(len(agent_list[i].neighbors)):
-                                # print("Agent",i,"keys:", j)                               # list the numbers
-                                # print("keys", list(agent_list[i].neighbors.keys())[j])
-                                coming_list_element = list(agent_list[i].neighbors.values())[j]
-                                coming_list_element.append(i)
+                            # for each neighbor cell that gets the bid:
+                            # collect offers at one location point(all_bids)
+                            # ex: key(cell), value(bids) ---> (1, 1), [[1.4142135623730951, 0.01860807318911967, 0], [1.0, 0.03333333333333333, 1]]
+                            for j, k in agent_list[i].neighbors.items():
                                 try:
-                                    all_bids[list(agent_list[i].neighbors.keys())[j]].append(coming_list_element)
+                                    all_bids[j].append(k)
                                 except:
-                                    all_bids[list(agent_list[i].neighbors.keys())[j]] = [coming_list_element]
-                        # print("ALLLLLLLL",all_bids)
-                        # print("ALL KEYS", all_bids.keys())
-                        # print("ALL VALUES", all_bids.values())
+                                    all_bids[j] = [k]
+                                print(j, all_bids[j])
+                        
+
+
+
+                        # which cell is won by which agent
                         for x in all_bids:
-                            # print("for the cells", x, "the bids are as follows:", all_bids[x])
+                            print("for the cells", x, "the bids are as follows:", all_bids[x])
                             a = np.array(all_bids[x])
                             highestbid = a.max(axis=0)[1]
                             # print("here is the highest_bid", highestbid)
@@ -290,31 +269,26 @@ def main():
                                 agent_list[all_bids[x][highestbid_index][2]].win_cells[x] = highestbid
                         # for i in agent_list:
                         #     print("Agent",i,"win cells with their bids:", agent_list[i].win_cells)
-
                         agent_locs.clear()
                         
+
+
+
+                        # among the win cells where the agent should move
                         for i in agent_list:
                             if agent_list[i].win_cells:
-                                # print("\n\n******1here")
+                                
                                 max_item_value = max(agent_list[i].win_cells.values())
                                 tup = [k for k, v in agent_list[i].win_cells.items() if v == max_item_value]
-                                # print(len(tup))
+                                
                                 if len(tup) > 1:
                                     ll = random.choice(tup)
                                     row_val, column_val = ll
-                                    # print(ll)
-
                                 else:
-                                    # print(tup[0])
                                     row_val, column_val = tup[0]
-                                
-                                # row_val, column_val = tup
-                                # print("&&&&&&&&&&&&")
+
                                 # print("where is the agent currently:", agent_list[i].row, agent_list[i].column)
                                 distance_of_move = agent_list[i].calc_won_distance(row_val, column_val)
-
-                                # print("hurr1", (distance_of_move + 5))
-                                # print("hurr2", agent_list[i].start_energy)
 
                                 if(agent_list[i].start_energy >= (distance_of_move + 5)):
                                     grid[row_val][column_val].agent_id = i
@@ -345,23 +319,17 @@ def main():
                         
                         
                         
-                        # print("****************************\n\nclearing...")
+                        
                         all_bids.clear()
                         for i in agent_list:
                             agent_list[i].win_cells.clear()
 
-                        # print("huu")
-                        # pygame.image.save(screen, "final.jpg")
                         if(all_areas_explored | all_dics_are_empty):
-                            # which one is true
-                            # end = time.time()
                             print("all_areas_explored",all_areas_explored)
                             print("all_dics_are_empty",all_dics_are_empty)
-                            # print("Testing2")
-                            # pygame.image.save(screen, "final.png")
                             break
-                            # pygame.quit()   # we are done so we exit this loop
-                            # sys.exit()
+                            
+                            
                         
                         # step_counter = step_counter+1
                     # pygame.image.save(screen, "final.png")
