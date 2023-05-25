eaf25bc1bb3219d174f998e258f9aab702cbcc14
direct-lab <taylor.anderson@usu.edu>
2023-05-25 01:48:16 -0600

    starting big run with following parameters

--- parameters_cfg.py
+++ parameters_cfg.py
@@ -29,35 +29,35 @@ class Parameters:
 
         # The number of agents in the experiment
         # self.agent_count_list = list(range(2,10,2))
-        # self.agent_count_list = [4,8,12]
-        self.agent_count_list = [12]
+        self.agent_count_list = [4,8,12]
+        # self.agent_count_list = [12]
         assert np.array(self.agent_count_list).max() <13, "The number of agents should be less than 13"
         
         # iteration_repeat_experiment will be used to repeat the experiment
-        # self.iteration_repeat_experiment = list(range(0, 1))
-        self.iteration_repeat_experiment = [1]
+        self.iteration_repeat_experiment = list(range(0, 30))
+        # self.iteration_repeat_experiment = [1]
 
-        # self.min_rom_size = [4,12,20]
-        self.min_rom_size = [4]
+        self.min_rom_size = [4,12,20]
+        # self.min_rom_size = [4]
         # self.min_rom_size = [3,6,9,12,30]
 
         self.Method_list = [
-            # Frontier_Random,
-            # Frontier_Closest,
-            # Unknown_Random,
-            # Unknown_Closest,
+            Frontier_Random,
+            Frontier_Closest,
+            Unknown_Random,
+            Unknown_Closest,
 
             # @@@@@@@@ Voronoi_Frontier_Random,
             # @@@@@@@@ Voronoi_Frontier_Closest,
             Voronoi_Frontier_Help_Closest,
-            # Voronoi_Frontier_Help_Random,
+            Voronoi_Frontier_Help_Random,
             
-            # Decision_Frontier_Closest,
-            # Decay_Epsilon_Greedy_Unknown,
-            # Decay_Epsilon_Greedy_Frontier,
+            Decision_Frontier_Closest,
+            Decay_Epsilon_Greedy_Unknown,
+            Decay_Epsilon_Greedy_Frontier,
             # Epsilon_Greedy_Unknown,
             # Epsilon_Greedy_Frontier,
-            # GameTheory,
+            GameTheory,
 
             # DarpVorOnly,
             # DarpMST,
@@ -69,25 +69,25 @@ class Parameters:
 
         self.Start_scenario_list = [
             # # Manual_Start,
-            # Rand_Start,
-            # Edge_Start,
+            Rand_Start,
+            Edge_Start,
             Top_Left_Start,
-            # Center_Start,
-            # Distributed_Start,
+            Center_Start,
+            Distributed_Start,
             ]
         self.Start_Goal_list= [
             # # Manual_Start,
-            # Rand_Start,
-            # Edge_Start,
-            # Top_Left_Start,
-            # Center_Start,
+            Rand_Start,
+            Edge_Start,
+            Top_Left_Start,
+            Center_Start,
             Distributed_Start,
             ]
         
         self.Robot_Loss = [
             Agent,
-            # Unrecoverable,
-            # Disrepair,
+            Unrecoverable,
+            Disrepair,
         ]
 
 
